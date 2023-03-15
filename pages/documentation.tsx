90e02619ec199698768889e65e1c4e65743e2131
Bamschool <87596687+Bamschool@users.noreply.github.com>
2023-03-16 08:06:55 +0900

    f

--- pages/documentation.tsx
+++ pages/documentation.tsx
@@ -5,69 +5,22 @@ import { useRouter } from "next/router";
 import client from "../client";
 import { gql } from "@apollo/client";
 import { data } from "autoprefixer";
-
+import { getAllDocumentationsForHome } from "../lib/api";
 import Layout from "../components/blog/layout";
 import Sidebar from "../components/documentation/sidebar";
 import React, { useState } from "react";
 import SecondNavbar from "../components/documentation/secondNavbar";
 
-export const getStaticProps = async ({ preview = false }) => {
-  const { data } = await client.query({
-    query: gql`
-      query AllDocumentations {
-        documentations(
-          first: 20
-          where: { orderby: { field: DATE, order: DESC } }
-        ) {
-          edges {
-            node {
-              title
-              excerpt
-              slug
-              date
-              featuredImage {
-                node {
-                  sourceUrl
-                }
-              }
-              author {
-                node {
-                  name
-                  firstName
-                  lastName
-                  avatar {
-                    url
-                  }
-                }
-              }
-              categories {
-                nodes {
-                  name
-                }
-              }
-            }
-          }
-        }
-      }
-    `,
-  });
+export async function getStaticProps({ preview = false }) {
+  const allDocumentations = await getAllDocumentationsForHome(preview);
 
   return {
-    props: {
-      data,
-      preview,
-      myexampleprop: "test",
-    },
+    props: { allDocumentations },
     revalidate: 10,
   };
-};
-
-interface DocumentationProps {
-  data: any;
-  children?: React.ReactNode;
 }
 
-export default function documentation(props: DocumentationProps) {
+export default function documentation({ allDocumentations }) {
   const [sidebarOpen, setSidebarOpen] = useState(false);
   return (
     <div className="grid min-h-screen grid-rows-header">
@@ -80,13 +33,102 @@ export default function documentation(props: DocumentationProps) {
         <Sidebar
           open={sidebarOpen}
           setOpen={setSidebarOpen}
-          allDocumentations={props.data.documentations.edges}
+          allDocumentations={allDocumentations}
         />
-        {props.children}
       </div>{" "}
     </div>
   );
 }
+
+// import Head from "next/head";
+// import { GetStaticProps } from "next";
+
+// import { useRouter } from "next/router";
+// import client from "../client";
+// import { gql } from "@apollo/client";
+// import { data } from "autoprefixer";
+
+// import Layout from "../components/blog/layout";
+// import Sidebar from "../components/documentation/sidebar";
+// import React, { useState } from "react";
+// import SecondNavbar from "../components/documentation/secondNavbar";
+
+// export const getStaticProps = async ({ preview = false }) => {
+//   const { data } = await client.query({
+//     query: gql`
+//       query AllDocumentations {
+//         documentations(
+//           first: 20
+//           where: { orderby: { field: DATE, order: DESC } }
+//         ) {
+//           edges {
+//             node {
+//               title
+//               excerpt
+//               slug
+//               date
+//               featuredImage {
+//                 node {
+//                   sourceUrl
+//                 }
+//               }
+//               author {
+//                 node {
+//                   name
+//                   firstName
+//                   lastName
+//                   avatar {
+//                     url
+//                   }
+//                 }
+//               }
+//               categories {
+//                 nodes {
+//                   name
+//                 }
+//               }
+//             }
+//           }
+//         }
+//       }
+//     `,
+//   });
+
+//   return {
+//     props: {
+//       data,
+//       preview,
+//       myexampleprop: "test",
+//     },
+//     revalidate: 10,
+//   };
+// };
+
+// interface DocumentationProps {
+//   data: any;
+//   children?: React.ReactNode;
+// }
+
+// export default function documentation(props: DocumentationProps) {
+//   const [sidebarOpen, setSidebarOpen] = useState(false);
+//   return (
+//     <div className="grid min-h-screen grid-rows-header">
+//       <div>
+//         <SecondNavbar
+//           onMenuButtonClick={() => setSidebarOpen((prev) => !prev)}
+//         />
+//       </div>
+//       <div className="grid md:grid-cols-sidebar">
+//         <Sidebar
+//           open={sidebarOpen}
+//           setOpen={setSidebarOpen}
+//           allDocumentations={props.data.documentations.edges}
+//         />
+//         {props.children}
+//       </div>{" "}
+//     </div>
+//   );
+// }
 
 // import React, { useState } from "react";
 // import { useRouter } from "next/router";
--- pages/documentation/[slug].tsx
+++ pages/documentation/[slug].tsx
@@ -1,116 +1,187 @@
-import React, { useState } from "react";
-import client from "../../client";
-import { gql } from "@apollo/client";
+import { useRouter } from "next/router";
+import ErrorPage from "next/error";
+import Head from "next/head";
+import DocumentationBody from "../../components/documentation/documentation-body";
+
+import {
+  getAllDocumentationWithSlug,
+  getDocumentationAndMorePosts,
+} from "../../lib/api";
+import { CMS_NAME } from "../../lib/constants";
 import Sidebar from "../../components/documentation/sidebar";
 
-import DocumentationBody from "../../components/documentation/documentation-body";
-import SecondNavbar from "../../components/documentation/secondNavbar";
-export async function getStaticPaths() {
-  const { data } = await client.query({
-    query: gql`
-      query AllDocumentations {
-        documentations {
-          nodes {
-            slug
-          }
-        }
-      }
-    `,
-  });
+export default function Post({ documentation, documentations, preview }) {
+  const router = useRouter();
+  const moreDocumentations = documentations?.edges;
 
-  return {
-    paths: data.documentations.nodes.map((doc) => `/documentation/${doc.slug}`),
-    fallback: false,
-  };
-}
+  if (!router.isFallback && !documentation?.slug) {
+    return <ErrorPage statusCode={404} />;
+  }
 
-export const getStaticProps = async ({ params, preview = false }) => {
-  const { data } = await client.query({
-    query: gql`
-      query Documentation($slug: String!) {
-        documentation: documentationBy(slug: $slug) {
-          title
-          content
-        }
-        documentations(
-          first: 20
-          where: { orderby: { field: DATE, order: DESC } }
-        ) {
-          edges {
-            node {
-              title
-              excerpt
-              slug
-              date
-              featuredImage {
-                node {
-                  sourceUrl
-                }
-              }
-              author {
-                node {
-                  name
-                  firstName
-                  lastName
-                  avatar {
-                    url
-                  }
-                }
-              }
-              categories {
-                nodes {
-                  name
-                }
-              }
-            }
-          }
-        }
-      }
-    `,
-    variables: { slug: params.slug },
-  });
-
-  return {
-    props: {
-      data,
-      preview,
-      myexampleprop: "test",
-    },
-  };
-};
-
-interface DocumentationProps {
-  data: any;
-  children?: React.ReactNode;
-}
-
-function jonghwan(props: DocumentationProps) {
-  const [sidebarOpen, setSidebarOpen] = useState(false);
-  const { data } = props;
-  const documentation = data.documentation;
   return (
-    <div className="grid min-h-screen grid-rows-header ">
-      <div>
-        <SecondNavbar
-          onMenuButtonClick={() => setSidebarOpen((prev) => !prev)}
-        />
-      </div>
-      <div className="grid md:grid-cols-sidebar">
-        <Sidebar
-          open={sidebarOpen}
-          setOpen={setSidebarOpen}
-          allDocumentations={props.data.documentations.edges}
-        />
-        <DocumentationBody
-          content={documentation.content}
-          title={documentation.title}
-        />
-      </div>{" "}
+    <div>
+      {documentation && (
+        <DocumentationBody content={documentation.content} title={undefined} />
+      )}
     </div>
   );
 }
 
-export default jonghwan;
+export async function getStaticProps({ params, preview = false, previewData }) {
+  const data = await getDocumentationAndMorePosts(
+    params?.slug,
+    preview,
+    previewData
+  );
+
+  if (!data.documentation) {
+    return { notFound: true };
+  }
+
+  return {
+    props: {
+      preview,
+      documentation: data.documentation,
+      documentations: data.documentations,
+    },
+    revalidate: 10,
+  };
+}
+
+export async function getStaticPaths() {
+  const allDocumentations = await getAllDocumentationWithSlug();
+
+  return {
+    paths:
+      allDocumentations.edges.map(
+        ({ node }) => `/documentation/${node.slug}`
+      ) || [],
+    fallback: true,
+  };
+}
+
+// import React, { useState } from "react";
+// import client from "../../client";
+// import { gql } from "@apollo/client";
+// import Sidebar from "../../components/documentation/sidebar";
+
+// import DocumentationBody from "../../components/documentation/documentation-body";
+// import SecondNavbar from "../../components/documentation/secondNavbar";
+// export async function getStaticPaths() {
+//   const { data } = await client.query({
+//     query: gql`
+//       query AllDocumentations {
+//         documentations {
+//           nodes {
+//             slug
+//           }
+//         }
+//       }
+//     `,
+//   });
+
+//   return {
+//     paths: data.documentations.nodes.map((doc) => `/documentation/${doc.slug}`),
+//     fallback: false,
+//   };
+// }
+
+// export const getStaticProps = async ({ params, preview = false }) => {
+//   const { data } = await client.query({
+//     query: gql`
+//       query Documentation($slug: String!) {
+//         documentation: documentationBy(slug: $slug) {
+//           title
+//           content
+//         }
+//         documentations(
+//           first: 20
+//           where: { orderby: { field: DATE, order: DESC } }
+//         ) {
+//           edges {
+//             node {
+//               title
+//               excerpt
+//               slug
+//               date
+//               featuredImage {
+//                 node {
+//                   sourceUrl
+//                 }
+//               }
+//               author {
+//                 node {
+//                   name
+//                   firstName
+//                   lastName
+//                   avatar {
+//                     url
+//                   }
+//                 }
+//               }
+//               categories {
+//                 nodes {
+//                   name
+//                 }
+//               }
+//             }
+//           }
+//         }
+//       }
+//     `,
+//     variables: { slug: params.slug },
+//   });
+
+//   return {
+//     props: {
+//       data,
+//       preview,
+//       myexampleprop: "test",
+//     },
+//   };
+// };
+
+// interface DocumentationProps {
+//   data: any;
+//   children?: React.ReactNode;
+// }
+
+// function jonghwan(props: DocumentationProps) {
+//   const [sidebarOpen, setSidebarOpen] = useState(false);
+//   const { data } = props;
+//   const documentation = data.documentation;
+
+//   if (!documentation) {
+//     return (
+//       <div>
+//         <h1>Error: The requested documentation was not found.</h1>
+//       </div>
+//     );
+//   }
+
+//   return (
+//     <div className="grid min-h-screen grid-rows-header ">
+//       <div>
+//         <SecondNavbar
+//           onMenuButtonClick={() => setSidebarOpen((prev) => !prev)}
+//         />
+//       </div>
+//       <div className="grid md:grid-cols-sidebar">
+//         <Sidebar
+//           open={sidebarOpen}
+//           setOpen={setSidebarOpen}
+//           allDocumentations={props.data.documentations.edges}
+//         />
+//         <DocumentationBody
+//           content={documentation.content}
+//           title={documentation.title}
+//         />
+//       </div>{" "}
+//     </div>
+//   );
+// }
+
+// export default jonghwan;
 
 // import React from "react";
 // import client from "client";
